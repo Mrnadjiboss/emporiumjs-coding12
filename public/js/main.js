4f59332e2f9f660b2cf2f09cdfd73801913f8c9b
nadjib <thenadjib@gmail.com>
2020-02-17 11:20:46 +0100

    black and white navbar

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,19 @@ let btnBlack = document.getElementsByClassName("btnBlack")[0]
 let body =  document.querySelector("body")
 let titleEmporium = document.getElementsByClassName("title")[0] 
 let nav = document.getElementsByClassName("laNav")[0]
+let image1Section3 = document.getElementsByClassName("img1")[0]
+let image2Section3 = document.getElementsByClassName("img2")[0]
+let image3Section3 = document.getElementsByClassName("img3")[0]
 
 btnWhite.addEventListener("click", () => {
     body.style.backgroundColor = "black"
+    nav.setAttribute("class", " navbar navbar-expand-lg navbar-dark bg-dark")
+    body.style.color = "white"
+    
     titleEmporium.setAttribute("class", "text-white")
+    image1Section3.style.border = "black solid 10px"
+    image2Section3.style.border = "black solid 10px"
+    image3Section3.style.border = "black solid 10px"
     
     
     
@@ -16,10 +25,13 @@ btnWhite.addEventListener("click", () => {
 btnBlack.addEventListener("click", () => {
     body.style.backgroundColor = "white"
     titleEmporium.setAttribute("class", "text-dark")
-    h1.style.color = "white"
-    nav.setAttribute("class", "navbar-dark bg-secondary")
-    
-    
+    nav.removeAttribute("class", "navbar-light bg-white")
+    body.style.color = "black"
+   
+    nav.setAttribute("class", " navbar navbar-expand-lg navbar-light bg-light")
+    image1Section3.style.border = "white solid 10px"
+    image2Section3.style.border = "white solid 10px"
+    image3Section3.style.border = "white solid 10px"
     
 })
 
