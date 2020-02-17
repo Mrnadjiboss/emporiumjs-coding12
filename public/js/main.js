545b4b2f377a1e93340c739a0359fee7b22183cb
nadjib <thenadjib@gmail.com>
2020-02-17 15:09:57 +0100

    form

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,31 @@
+
 let btnWhite =  document.getElementsByClassName("btnWhite")[0]
 let btnBlack = document.getElementsByClassName("btnBlack")[0]
+
+// body 
+
 let body =  document.querySelector("body")
+
+// titre
 let titleEmporium = document.getElementsByClassName("title")[0] 
 let nav = document.getElementsByClassName("laNav")[0]
+
+// image section 
 let image1Section3 = document.getElementsByClassName("img1")[0]
 let image2Section3 = document.getElementsByClassName("img2")[0]
 let image3Section3 = document.getElementsByClassName("img3")[0]
+
+let modale = document.getElementsByClassName("modale")[0]
+modale.style.display = "none"
+let modale2 = document.getElementsByClassName("modale2")[0]
+modale2.style.display = "none"
+
+
+
+let btnConnexion =  document.getElementsByClassName("btnConnexion")[0]
+let btnQuit = document.getElementsByClassName("quit")[0]
+
+let btnInscription = document.getElementsByClassName("btnInscription")[0]
 
 btnWhite.addEventListener("click", () => {
     body.style.backgroundColor = "black"
@@ -34,4 +54,23 @@ btnBlack.addEventListener("click", () => {
     image3Section3.style.border = "white solid 10px"
     
 })
+
+btnConnexion.addEventListener("click", () => {
+    modale.style.display = "block"
+
+
+})
+
+btnQuit.addEventListener("click", () => {
+    modale.style.display = "none"
+
+
+})
+
+btnInscription.addEventListener("click", () => {
+    modale2.style.display = "block"
+
+
+})
+
 
